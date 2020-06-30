69f826bdf4ae29ebcafd272af33c645e68e4c8fb
Spencer <zqian0336@gmail.com>
2020-06-29 17:38:08 -0700

    react commit

---
+++ build/precache-manifest.cb3e89fd28f7ebb977a0762104651b4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197e6fe07ca89a229c5cb94c1d434829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4979baedca88c17a9604",
+    "url": "/static/css/main.04b6fa0e.chunk.css"
+  },
+  {
+    "revision": "4a906f98dda9b516508d",
+    "url": "/static/js/2.727eb665.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.727eb665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4979baedca88c17a9604",
+    "url": "/static/js/main.f44a1d5d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a2215d9743567489796d05d96f0c512a",
+    "url": "/static/media/showcase.a2215d97.jpg"
+  },
+  {
+    "revision": "efeed92d2f03fd4bc4f3a271b2feae4c",
+    "url": "/static/media/spinner.efeed92d.gif"
+  }
+]);
